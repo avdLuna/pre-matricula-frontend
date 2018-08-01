74887140625d4617e795ea94777652ed9e2ec169
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-08-01 00:16:13 -0300

    Update login button

--- src/components/login/login_button.js
+++ src/components/login/login_button.js
@@ -3,44 +3,18 @@ import  GoogleLogin  from 'react-google-login';
 
 class LoginButton extends Component {
 
-/*
-  onSubmit(response){
-    const token = response.Zi.access_token;
-    const refreshToken = response.Zi.access_token;
-    localStorage.setItem('token', token);
-    localStorage.setItem('refreshToken', refreshToken);
-  }
-  onSubmit(response){
-    fetch("http://localhost:8080/user")
-    .then(data => console.log(data))
-  }
-*/
-
   onSubmit(){
     console.log("clicou");
-    const myWindow = window.open("http://localhost:8080/login", "myWindow", "width=700, height=700");
+    var loginWindow = window.open("http://localhost:8080/login", "_blank", "width=700, height=700");
     const timer = setInterval(function() {   
-      if(myWindow.closed) {  
+      if(loginWindow.closed) {  
           clearInterval(timer);  
-          alert('closed');
-      }  
+          console.log(loginWindow.Cookies);
+      }
     }, 1000);
   }
 
   render(){
-    /*
-    const responseGoogle = (response) => {
-      //this.onSubmit(response);
-      console.log(response);
-      <GoogleLogin
-         clientId="824246316091-l1dhfehnq1c4cs1k3aaacsk79aufictv.apps.googleusercontent.com"
-         className="btn btn-primary"
-         buttonText="Login"
-         onSuccess={responseGoogle}
-         onFailure={responseGoogle}
-       />
-    }
-    */
       return(
        <button type="button" className="btn btn-primary" onClick={() => this.onSubmit()}>Login</button>
       );
